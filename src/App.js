9fe47e05e286fa1b9bbec772ee8c347199220bca
Region60 <_max_kot@mail.ru>
2022-05-08 00:26:43 +0300

    header ready 100%

--- src/App.js
+++ src/App.js
@@ -7,29 +7,28 @@ import Catalogue from './components/catalogue/Catalogue'
 import Footer from './components/footer/Footer'
 import Calculator from './components/calculator/Calculator'
 import PreFooter from './components/prefooter/PreFooter'
+import tw from 'twin.macro';
 
 
 class App extends Component {
   render() {
     return (
       <AppContainer>
-
+        <Header />
+        
         <Wrapper>
-          <Header />
+         {/* <Intro />
+         <InfoBlock />
+         <Calculator />
+         <Catalogue />
+         <PreFooter/> */}
         </Wrapper>
-
-
-          {/*<Intro />*/}
-          {/*<InfoBlock />*/}
-          {/*<Calculator />*/}
-          {/*<Catalogue />*/}
-          {/*<PreFooter/>*/}
-          {/*<Footer />*/}
+        {/*<Footer />*/}
       </AppContainer>
     );
   }
 }
 const Wrapper = tw.div`w-[1000px]`
-const AppContainer = tw.div`flex w-screen justify-center`
+const AppContainer = tw.div`flex flex-col w-screen justify-center`
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import classes from './Header.module.css'
 import logo from '../../assets/logo.png'
 import linc from '../../assets/linc.png'
 import fcb_1 from '../../assets/fcb_1.png'
@@ -9,61 +8,67 @@ import tw from 'twin.macro';
 
 const Header = (props) => {
     return (
-        <Test >
-            d
-            {/*<HeaderBody>*/}
-            {/*    <HeaderLogo>*/}
-            {/*        <LogoImage src={logo} />*/}
-            {/*        <LogoSeparator></LogoSeparator>*/}
-            {/*        <LogoName>*/}
-            {/*            your<br />*/}
-            {/*            door*/}
-            {/*        </LogoName>*/}
-            {/*    </HeaderLogo>*/}
-            {/*    <HeaderMenu>*/}
-            {/*        <HeaderMenuLi>*/}
-            {/*            <HeaderMenuCatalogue href="#">КАТАЛОГ</HeaderMenuCatalogue>*/}
-            {/*        </HeaderMenuLi>*/}
-            {/*        <HeaderMenuLi>*/}
-            {/*            <HeaderMenuContakts href="#">КОНТАКТЫ</HeaderMenuContakts>*/}
-            {/*        </HeaderMenuLi>*/}
-            {/*        <HeaderMenuLi>*/}
-            {/*            <HeaderMenuCalculator href="#">РАСЧИТАТЬ СТОИМОСТЬ</HeaderMenuCalculator>*/}
-            {/*        </HeaderMenuLi>*/}
-            {/*        <HeaderMenuLi>*/}
-            {/*            <HeaderMenuLink src={linc} />*/}
-            {/*        </HeaderMenuLi>*/}
-            {/*        <HeaderMenuLi>*/}
-            {/*            <HeaderMenuShare href="#">ПОДЕЛИТЬСЯ</HeaderMenuShare>*/}
-            {/*        </HeaderMenuLi>*/}
-            {/*    </HeaderMenu>*/}
-            {/*</HeaderBody>*/}
-            {/*<HeaderSeparator />*/}
-            {/*<Headerlinkwrapper>*/}
-            {/*    <Headerlinks>*/}
-            {/*        <LinkImg src={inst_1} alt="img" />*/}
-            {/*        <LinkImg src={fcb_1} alt="img" />*/}
-            {/*    </Headerlinks>*/}
+        <HeaderWrapper >
+            <HeaderBodyContainer>
+                <HeaderBody>
+                    <HeaderLogo>
+                        <LogoImage src={logo} />
+                        <LogoSeparator></LogoSeparator>
+                        <LogoName>
+                            your<br />
+                            door
+                        </LogoName>
+                    </HeaderLogo>
+                    <HeaderMenu>
+                        <div>
+                            <HeaderMenuLi>
+                                <HeaderMenuCatalogue href="#">КАТАЛОГ</HeaderMenuCatalogue>
+                            </HeaderMenuLi>
+                            <HeaderMenuLi>
+                                <HeaderMenuContakts href="#">КОНТАКТЫ</HeaderMenuContakts>
+                            </HeaderMenuLi>
+                        </div>
 
-            {/*</Headerlinkwrapper>*/}
-        </Test>
+                        <HeaderMenuLi>
+                            <HeaderMenuCalculator href="#">РАСЧИТАТЬ СТОИМОСТЬ</HeaderMenuCalculator>
+                        </HeaderMenuLi>
+                        <HeaderMenuLi>
+                            <HeaderMenuLink src={linc} />
+                            <HeaderMenuShare href="#">ПОДЕЛИТЬСЯ</HeaderMenuShare>
+                        </HeaderMenuLi>
+                    </HeaderMenu>
+                </HeaderBody>
+            </HeaderBodyContainer>
+            <HeaderLinkContainer>
+                <Headerlinkwrapper>
+                    <Headerlinks>
+                        <LinkImg src={inst_1} alt="img" />
+                        <LinkImg src={fcb_1} alt="img" />
+                    </Headerlinks>
+                </Headerlinkwrapper>
+            </HeaderLinkContainer>
+
+
+        </HeaderWrapper>
     )
 }
-const Test = tw.header`bg-red-500 h-40`;
-const HeaderBody = tw.div`flex justify-between items-center h-[56px]`
+
+const HeaderWrapper = tw.div`flex flex-col justify-center`
+const HeaderBodyContainer = tw.div`flex justify-center w-screen border-b border-black`
+const HeaderBody = tw.div`flex justify-between items-center w-[1200px] h-[56px]`
 const HeaderLogo = tw.div`flex justify-between max-w-[100px] min-w-[70px]`
 const LogoImage = tw.img`h-[24px] w-[24px]`
 const LogoSeparator = tw.div`inline-block bg-current w-[1px] h-[30px]`
 const LogoName = tw.div`text-right text-xs`
-const HeaderMenu = tw.ul`flex w-full`
+const HeaderMenu = tw.ul`flex justify-around w-full`
 const HeaderMenuLi = tw.li`inline-block`
-const HeaderMenuCatalogue = tw.div`  text-right w-[175px]`
-const HeaderMenuContakts = tw.div` text-right w-[100px]`
-const HeaderMenuCalculator = tw.div`text-center w-[700px]`
+const HeaderMenuCatalogue = tw.div`  text-right `
+const HeaderMenuContakts = tw.div` text-right pl-[10px]`
+const HeaderMenuCalculator = tw.div`text-center `
 const HeaderMenuLink = tw.img`inline-block h-[17px] w-[17px]`
-const HeaderMenuShare = tw.div`text-right w-[110px]`
-const HeaderSeparator = tw.div`w-full min-w-[1200px] h-[1px] bg-black mx-auto`
-const Headerlinkwrapper = tw.div`flex justify-end mx-auto w-[1000px] `
+const HeaderMenuShare = tw.div`inline-block pl-2 text-right`
+const HeaderLinkContainer = tw.div`flex justify-center w-full`
+const Headerlinkwrapper = tw.div`flex justify-end  w-[1030px] `
 const Headerlinks = tw.div`flex justify-between w-[94px] h-[64px]  shadow-md`
 const LinkImg = tw.img`w-[24px] h-[24px] left-[5px] mx-[11px] my-[20px]`
 
