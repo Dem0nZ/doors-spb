9b738b76a70cb5d4f8a94e67c0679ae73188f29c
Dem0nZ <cbpskov@gmail.com>
2022-05-14 17:12:07 +0300

    add prettier conf, prettier ignore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Header from './components/header/Header'
 import Intro from './components/intro/Intro'
 import InfoBlock from './components/infoblock/InfoBlock'
@@ -9,24 +9,22 @@ import PreFooter from './components/prefooter/PreFooter'
 import tw from 'twin.macro';
 
 
-class App extends Component {
-  render() {
-    return (
-      <AppContainer>
-         <Header />
+const App = () => {
+  return (
+    <AppContainer>
+      <Header/>
+      <Wrapper>
+        <Intro/>
+        <InfoBlock/>
+        <Calculator/>
+        <Catalogue/>
+        <PreFooter/>
+      </Wrapper>
+      <Footer/>
+    </AppContainer>
+  )
+};
 
-        <Wrapper>
-          <Intro />
-          <InfoBlock />
-            <Calculator />
-          <Catalogue />
-          <PreFooter /> 
-        </Wrapper>
-        <Footer />
-      </AppContainer>
-    );
-  }
-}
 const Wrapper = tw.div`flex flex-col justify-center max-w-[1000px]`
 const AppContainer = tw.div`flex flex-col items-center w-full h-full`
 
--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,36 +1,33 @@
-
 import React from 'react'
 import tw from 'twin.macro';
 
+const Calculator = () => {
+  return (
+    <Wrapper>
+      <Content>
+        <Title>
+          РАСЧИТАТЬ СТОИМОСТЬ
+        </Title>
+        <Description>
+          Тут разный текст Тут разный текст Тут
+          разный текст Тут разный текст Тут
+          разный текст Тут разный текст Тут
+          разный текст Тут разный текст Тут
+          разный текст Тут разный текст Тут ра
+        </Description>
+        <Button>
+          РАСЧИТАТЬ СТОИМОСТЬ
+        </Button>
+      </Content>
 
-const Calculator = (props) => {
-    return (
-        <CalculatorWrapper>
-            <CalculatorBody>
-            <CalculatorTitle>
-                РАСЧИТАТЬ СТОИМОСТЬ
-            </CalculatorTitle>
-            <CalculatorDescription>
-                Тут разный текст Тут разный текст Тут
-                разный текст Тут разный текст Тут
-                разный текст Тут разный текст Тут
-                разный текст Тут разный текст Тут
-                разный текст Тут разный текст Тут ра
-            </CalculatorDescription>
-            <CalculatorBtnCalc>
-            РАСЧИТАТЬ СТОИМОСТЬ
-            </CalculatorBtnCalc>
-    
-            </CalculatorBody>
-            
-        </CalculatorWrapper>
-    )
+    </Wrapper>
+  )
 }
 
-const CalculatorWrapper = tw.div`flex justify-center mt-[110px] w-full`
-const CalculatorBody = tw.div`flex-col`
-const CalculatorTitle = tw.div`flex text-5xl font-medium`
-const CalculatorDescription = tw.div`flex w-[550px] text-sm`
-const CalculatorBtnCalc = tw.button` mt-[30px] w-[400px] h-[60px] text-sm border`
+const Wrapper = tw.div`flex justify-center mt-[110px] w-full`
+const Content = tw.article`flex-col`
+const Title = tw.h1`flex text-5xl font-medium`
+const Description = tw.div`flex w-[550px] text-sm`
+const Button = tw.button` mt-[30px] w-[400px] h-[60px] text-sm border`
 
 export default Calculator
