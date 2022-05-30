579e16d790d75bafe07de1fae18cbcc2859471c4
Dem0nZ <cbpskov@gmail.com>
2022-05-30 15:03:19 +0300

    fix tw

---
+++ src/components/Calculator/local-components/PreviewDoors.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import tw from 'twin.macro';
+
+const PreviewDoors = ({data}) => {
+  return (
+    <div className='flex'>
+      <div  className='bg-red-700'>
+dffsdfsdfs
+      </div>
+      <div>
+        PRPERPEPRPERF
+      </div>
+    </div>
+  )
+}
+
+
+const ConstructorContainer = tw.div``
+
+export default PreviewDoors
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        blacked: '#161616',
+      }
+    },
+  },
+  plugins: [],
+}
