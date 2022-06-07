b2f8b3c8f99235636b21eede31ae5fa656145615
Dem0nZ <cbpskov@gmail.com>
2022-06-07 07:55:41 +0300

    delete unused components

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,24 +1,22 @@
-import React  from 'react'
+import React from 'react';
 import tw from 'twin.macro';
-import Catalog from '../Catalog/Catalog'
-import BenefitsBlock from './local-components/BenefitsBlock'
-import Intro from './local-components/Intro'
-import PreFooter from './local-components/PreFooter'
-import CalculatorBlock from './local-components/CalculatorBlock'
+import Catalog from '../Catalog/Catalog';
+import BenefitsBlock from './local-components/BenefitsBlock';
+import Intro from './local-components/Intro';
+import CalculatorBlock from './local-components/CalculatorBlock';
 
 const HomePage = () => {
-    return (
-        <Wrapper>
-            <Intro />
-            <BenefitsBlock />
-            <CalculatorBlock />
-            <Catalog />
-            {/*<PreFooter />*/}
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <Intro />
+      <BenefitsBlock />
+      <CalculatorBlock />
+      <Catalog />
+    </Wrapper>
+  );
+};
 
-const Wrapper = tw.div`flex flex-col pb-32 max-w-[1000px]`
+const Wrapper = tw.div`flex flex-col pb-32 max-w-[1000px]`;
 
 
-export default HomePage
+export default HomePage;
