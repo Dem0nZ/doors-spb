062162745eed2fc9b0dee786949eae089eacfe59
Dem0nZ <cbpskov@gmail.com>
2022-06-14 23:48:02 +0300

    contacts

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -4,122 +4,68 @@ import tw from 'twin.macro';
 import Map from './local-components/Map';
 import facade_view from '../../assets/office.jpg';
 
-const Contacts = () => {
+const Contacts = ({ contacts }) => {
 
   return (
     <Wrapper>
       <Container>
         <div>
-          <p className='text-3xl font-normal'>
-            КОНТАКТЫ
-          </p>
-          <p className=' text-5xl font-medium'>
-            МЕБЕЛЬ 178
-          </p>
+          <p className='text-3xl font-normal'>КОНТАКТЫ</p>
+          <p className='text-5xl font-medium'>МЕБЕЛЬ 178</p>
         </div>
-
         <ContactsInfo>
           <Column>
-            <ColumnTitle>
-              <ColumnLine />
-              <div>
-                ПРИЕЗЖАЙТЕ
-              </div>
-            </ColumnTitle>
-            <ColumnText>
-              <p>
-                Санкт-Петербург
-              </p>
-              <p>
-                Коттеджный посёлок
-              </p>
-              Северные террасы, 22
-            </ColumnText>
+            <div className='flex'><ColumnLine />ПРИЕЗЖАЙТЕ</div>
+            <p>Санкт-Петербург, Коттеджный посёлок Северные террасы, 22</p>
           </Column>
           <Column>
-            <ColumnTitle>
-              <ColumnLine />
-              <div>
-                ПИШИТЕ
-              </div>
-            </ColumnTitle>
-            <ColumnText>
-              <a href='mailto:info@info@mebel-178.ru'>info@info@mebel-178.ru</a>
-            </ColumnText>
+            <div className='flex'><ColumnLine />ПИШИТЕ</div>
+            <a href='mailto:info@info@mebel-178.ru'>info@info@mebel-178.ru</a>
           </Column>
           <Column>
-            <ColumnTitle>
-              <ColumnLine />
-              <div>
-                ЗВОНИТЕ
-              </div>
-            </ColumnTitle>
-            <ColumnText>
-              <a href='tel:RU79119978257'>RU +7 911 997 82 57</a>
-            </ColumnText>
+            <div className='flex'><ColumnLine />ЗВОНИТЕ</div>
+            <a href='tel:RU79119978257'>RU +7 911 997 82 57</a>
           </Column>
-
         </ContactsInfo>
         <Location>
-          <img className='' src={facade_view} alt='img' />
+          <img src={facade_view} alt='img' />
           <Map />
         </Location>
-
         <Feedback>
           <div>
-            <FeedbackTitle>
-              <p>ВЫ ВСЕГДА МОЖЕТЕ</p>
-              <p>СВЯЗАТЬСЯ С НАМИ</p>
-            </FeedbackTitle>
-            <Questions>
-              <div>
-                <p>Решили присоединиться к нашей команде или </p>
-                <p>задать вопрос?</p>
-
-              </div>
-              <div className='mt-8'>
-                <p>Заполните форму, и наш менеджер свяжется с</p>
-                <p>Вами в ближайшее время</p>
-              </div>
-            </Questions>
+            <FeedbackTitle>ВЫ ВСЕГДА МОЖЕТЕ СВЯЗАТЬСЯ С НАМИ</FeedbackTitle>
+            <div>
+              <p>Решили присоединиться к нашей команде или задать вопрос?</p>
+              <p>Заполните форму, и наш менеджер свяжется с Вами в ближайшее время.</p>
+            </div>
           </div>
           <InputWrapper>
-            <div className='border-b-2'><Input className='outline-none' placeholder='Имя' bordered={false} /></div>
-            <div className='border-b-2'><Input className='outline-none' placeholder='Фамилия' bordered={false} /></div>
-            <div className='col-span-2 border-b-2'><Input className='outline-none' placeholder='Ваш Email*'
-                                                          bordered={false} /></div>
-            <div className='col-span-2 border-b-2'><Input className='outline-none' placeholder='Ваш телефон*'
-                                                          bordered={false} /></div>
-            <div className='col-span-2 border-b-2'><Input className='outline-none' placeholder='Ваше сообщение'
-                                                          bordered={false} /></div>
-            <div className='col-span-2 mt-10'><ButtonSend>ОТПРАВИТЬ</ButtonSend></div>
+            <Input className='border-b-2 outline-none' placeholder='Имя' bordered={false} />
+            <Input className='border-b-2 outline-none' placeholder='Фамилия' bordered={false} />
+            <Input className='col-span-2 border-b-2 outline-none' placeholder='Ваш Email*'
+                   bordered={false} />
+            <Input className='col-span-2 border-b-2 outline-none' placeholder='Ваш телефон*'
+                   bordered={false} />
+            <Input className='col-span-2 border-b-2 outline-none' placeholder='Ваше сообщение'
+                   bordered={false} />
+            <ButtonSend>ОТПРАВИТЬ</ButtonSend>
           </InputWrapper>
-
         </Feedback>
-
       </Container>
     </Wrapper>
-
   );
-
 };
-const Wrapper = tw.div` flex w-screen h-screen min-h-[1500px] justify-center`;
-const Container = tw.div`flex flex-col w-[1000px] mt-40`;
 
+const Wrapper = tw.div` flex w-screen justify-center`;
+const Container = tw.div`flex flex-col max-w-[1000px] mt-40`;
 const ContactsInfo = tw.div`grid grid-cols-[1fr 20% 20%] mt-16`;
 const Column = tw.div`flex flex-col mt-8 pr-5`;
-const ColumnTitle = tw.div`flex`;
-const ColumnText = tw.div`flex flex-col mt-[40px] leading-6`;
 const ColumnLine = tw.div`w-8 h-[1px] mt-[10px] mr-[14px] bg-black`;
-
-const Location = tw.div`flex justify-between gap-16 mt-10 `;
-
+const Location = tw.div`flex justify-between gap-16 mt-10`;
 const Feedback = tw.div`flex justify-between mt-32`;
-const FeedbackTitle = tw.div`text-4xl font-medium `;
-const Questions = tw.div`mt-8`;
-
+const FeedbackTitle = tw.p`text-4xl font-medium `;
 const InputWrapper = tw.div`grid grid-cols-2 gap-y-12 gap-x-4 `;
-const ButtonSend = tw.div`border text-center py-4 hover:bg-gray-300 active:bg-gray-500`;
+const ButtonSend = tw.div`col-span-2 mt-10 border text-center py-4 hover:bg-gray-300 active:bg-gray-500`;
 
 
 export default Contacts;
--- src/pages/Contacts/local-components/Map.js
+++ src/pages/Contacts/local-components/Map.js
@@ -2,23 +2,22 @@ import React from 'react';
 import DG from '2gis-maps';
 import { useEffect } from 'react';
 
-
 const Map = () => {
-    useEffect(()=>{
-        const map = DG.map('map', {
-            'center': [ 60.181844, 30.364242],
-            'zoom': 15,
-            'zoomControl': false,
-            'fullscreenControl': false,
-        });
-        DG.marker([ 60.181844, 30.364242])
-            .addTo(map)
-            .bindLabel('Офис', {
-                static: true
-            });
-        
-    }, [])
-    return (<div id='map' className='w-1000 h-450 bg-red-300 sm:w-full sm:h-96'></div>);
+  useEffect(() => {
+    const map = DG.map('map', {
+      'center': [60.181844, 30.364242],
+      'zoom': 15,
+      'zoomControl': false,
+      'fullscreenControl': false,
+    });
+    DG.marker([60.181844, 30.364242])
+      .addTo(map)
+      .bindLabel('Офис', {
+        static: true,
+      });
+
+  }, []);
+  return (<div id='map' className='w-1000 h-450 bg-red-300 sm:w-full sm:h-96'></div>);
 };
 
 export default Map;
