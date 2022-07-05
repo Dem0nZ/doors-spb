3f7fabbfa567b108f3e55ca8bd330351bd1cae52
Dem0nZ <cbpskov@gmail.com>
2022-07-05 21:45:44 +0300

    refactoring catalog to react.FC

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -3,6 +3,7 @@ import tw from 'twin.macro';
 import Slider from 'react-slick';
 import { Screen0, Screen1, Screen2, Screen3, Screen4, Screen5 } from './screens';
 import { ArrowIcon } from '../../assets';
+import { v4 as uuidv4 } from 'uuid';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import * as Scroll from 'react-scroll';
@@ -10,72 +11,66 @@ import * as Scroll from 'react-scroll';
 const Link = Scroll.Link;
 const windowInnerWidth = document.documentElement.clientWidth < 1000 ? document.documentElement.clientWidth : 1000;
 
-export default class Catalog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-  }
+const buttons = [
+  'ШКАФЫ-КУПЕ ВСТРОЕННЫЕ',
+  'ПЕРЕГОРОДКИ МЕЖКОМНАТНЫЕ',
+  'ШКАФЫ-КУПЕ ОТДЕЛЬНО СТОЯЩИЕ',
+  'ДВЕРИ-КУПЕ',
+  'МАНСАРДНЫЕ ШКАФЫ',
+  'РАСПАШНЫЕ ШКАФЫ',
+];
 
-  next() {
-    this.slider.slickNext();
-  }
 
-  previous() {
-    this.slider.slickPrev();
-  }
+const Catalog = () => {
+  const slider = useRef();
+  const next = () => slider.current.SlickNext();
+  const previous = () => slider.current.slickPrev();
+  const goTo = (index) => slider.current.slickGoTo(index);
+  const settings = {
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+  };
+  const navButtons = buttons.map((element, index) => <SliderButton key={uuidv4()}
+                                                                   onClick={() => goTo(index)}>{element}</SliderButton>);
 
-  render() {
-    const settings = {
-      dots: false,
-      infinite: true,
-      arrows: false,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      adaptiveHeight: true,
-    };
-
-    return (
-      <Wrapper>
-        <UpperBlock>
-          <Title name='top-catalog'>КАТАЛОГ</Title>
-          <Nav>
-            <SliderButton onClick={() => this.slider.slickGoTo(0)}>ШКАФЫ-КУПЕ ВСТРОЕННЫЕ</SliderButton>
-            <SliderButton onClick={() => this.slider.slickGoTo(1)}>ПЕРЕГОРОДКИ МЕЖКОМНАТНЫЕ</SliderButton>
-            <SliderButton onClick={() => this.slider.slickGoTo(2)}>ШКАФЫ-КУПЕ ОТДЕЛЬНО СТОЯЩИЕ</SliderButton>
-            <SliderButton onClick={() => this.slider.slickGoTo(3)}>ДВЕРИ-КУПЕ</SliderButton>
-            <SliderButton onClick={() => this.slider.slickGoTo(4)}>МАНСАРДНЫЕ ШКАФЫ</SliderButton>
-            <SliderButton onClick={() => this.slider.slickGoTo(5)}>РАСПАШНЫЕ ШКАФЫ</SliderButton>
-          </Nav>
-        </UpperBlock>
-        <SliderBody className='flex w-full mt-10' style={{
-          width: windowInnerWidth - 48 + 'px',
-        }}>
-          <Slider ref={c => (this.slider = c)} {...settings}>
-            <Screen0 />
-            <Screen1 />
-            <Screen2 />
-            <Screen3 />
-            <Screen4 />
-            <Screen5 />
-          </Slider>
-          <SliderButtons>
-            <SliderNawDown>
-              <Link className='hover:bg-gray-300 active:bg-gray-500 transition-all duration-300'
-                      onClick={this.previous} to='top-catalog' spy={true} smooth={true} offset={50} duration={500}>
-                <ArrowIcon />
-              </Link>
-              <Link className='hover:bg-gray-300 active:bg-gray-500 rotate-180 transition-all duration-300'
-                      onClick={this.next} to='top-catalog' spy={true} smooth={true} offset={50} duration={500}>
-                <ArrowIcon />
-              </Link>
-            </SliderNawDown>
-          </SliderButtons>
-        </SliderBody>
-      </Wrapper>);
-  }
-}
+  return (
+    <Wrapper>
+      <UpperBlock>
+        <Title name='top-catalog'>КАТАЛОГ</Title>
+        <Nav>{navButtons}</Nav>
+      </UpperBlock>
+      <SliderBody className='flex w-full mt-10' style={{
+        width: windowInnerWidth - 48 + 'px',
+      }}>
+        <Slider ref={ref => (slider.current = ref)} {...settings}>
+          <Screen0 />
+          <Screen1 />
+          <Screen2 />
+          <Screen3 />
+          <Screen4 />
+          <Screen5 />
+        </Slider>
+        <SliderButtons>
+          <SliderNawDown>
+            <Link className='hover:bg-gray-300 active:bg-gray-500 transition-all duration-300'
+                  onClick={previous} to='top-catalog' spy={true} smooth={true} offset={50} duration={500}>
+              <ArrowIcon />
+            </Link>
+            <Link className='hover:bg-gray-300 active:bg-gray-500 rotate-180 transition-all duration-300'
+                  onClick={next} to='top-catalog' spy={true} smooth={true} offset={50} duration={500}>
+              <ArrowIcon />
+            </Link>
+          </SliderNawDown>
+        </SliderButtons>
+      </SliderBody>
+    </Wrapper>);
+};
+export default Catalog;
 
 const Wrapper = tw.div`flex flex-col justify-center w-full mt-12`;
 const UpperBlock = tw.div`flex flex-col`;
